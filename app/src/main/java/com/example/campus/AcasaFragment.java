323a00f23db026d44570d665864fea00c741ced1
Echipa7CTI <pascale_dorin@yahoo.com>
2020-08-22 02:25:06 +0300

    Camine

--- app/src/main/java/com/example/campus/AcasaFragment.java
+++ app/src/main/java/com/example/campus/AcasaFragment.java
@@ -13,8 +13,8 @@ import androidx.fragment.app.Fragment;
 
 public class AcasaFragment extends Fragment {
 
-    Button caminAbtn, caminBbtn, caminCbtn;
-    RelativeLayout caminArel, caminBrel, caminCrel;
+    Button caminAbtn, caminBbtn, caminCbtn, caminDbtn, caminEbtn, caminGbtn, caminHbtn, caminLSGbtn;
+    RelativeLayout caminArel, caminBrel, caminCrel, caminDrel, caminErel, caminGrel, caminHrel, caminLSGrel;
 
     @Nullable
     @Override
@@ -24,13 +24,28 @@ public class AcasaFragment extends Fragment {
         caminAbtn = view.findViewById(R.id.buton_A);
         caminBbtn = view.findViewById(R.id.buton_B);
         caminCbtn = view.findViewById(R.id.buton_C);
+        caminDbtn = view.findViewById(R.id.buton_D);
+        caminEbtn = view.findViewById(R.id.buton_E);
+        caminGbtn = view.findViewById(R.id.buton_G);
+        caminHbtn = view.findViewById(R.id.buton_H);
+        caminLSGbtn = view.findViewById(R.id.buton_LSG);
         caminArel = view.findViewById(R.id.rel_A);
         caminBrel = view.findViewById(R.id.rel_B);
         caminCrel = view.findViewById(R.id.rel_C);
+        caminDrel = view.findViewById(R.id.rel_D);
+        caminErel = view.findViewById(R.id.rel_E);
+        caminGrel = view.findViewById(R.id.rel_G);
+        caminHrel = view.findViewById(R.id.rel_H);
+        caminLSGrel = view.findViewById(R.id.rel_LSG);
 
         caminArel.setVisibility(View.GONE);
         caminBrel.setVisibility(View.GONE);
         caminCrel.setVisibility(View.GONE);
+        caminDrel.setVisibility(View.GONE);
+        caminErel.setVisibility(View.GONE);
+        caminGrel.setVisibility(View.GONE);
+        caminHrel.setVisibility(View.GONE);
+        caminLSGrel.setVisibility(View.GONE);
 
         // Buton Camin A -->  info
         caminAbtn.setOnClickListener(new View.OnClickListener() {
@@ -67,6 +82,71 @@ public class AcasaFragment extends Fragment {
                 }
                 else {
                     caminCrel.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        // Buton Camin D -->  info
+        caminDbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (caminDrel.getVisibility() == View.GONE) {
+                    caminDrel.setVisibility(View.VISIBLE);
+                }
+                else {
+                    caminDrel.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        // Buton Camin E -->  info
+        caminEbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (caminErel.getVisibility() == View.GONE) {
+                    caminErel.setVisibility(View.VISIBLE);
+                }
+                else {
+                    caminErel.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        // Buton Camin G -->  info
+        caminGbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (caminGrel.getVisibility() == View.GONE) {
+                    caminGrel.setVisibility(View.VISIBLE);
+                }
+                else {
+                    caminGrel.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        // Buton Camin H -->  info
+        caminHbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (caminHrel.getVisibility() == View.GONE) {
+                    caminHrel.setVisibility(View.VISIBLE);
+                }
+                else {
+                    caminHrel.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        // Buton Camin LSG -->  info
+        caminLSGbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (caminLSGrel.getVisibility() == View.GONE) {
+                    caminLSGrel.setVisibility(View.VISIBLE);
+                }
+                else {
+                    caminLSGrel.setVisibility(View.GONE);
                 }
             }
         });
