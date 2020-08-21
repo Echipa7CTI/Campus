25f784d937f52777fc92d09cbfe12536b80529fa
Echipa7CTI <pascale_dorin@yahoo.com>
2020-08-21 19:10:34 +0300

    test1

--- app/src/main/java/com/example/campus/AcasaFragment.java
+++ app/src/main/java/com/example/campus/AcasaFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,9 +13,50 @@ import androidx.fragment.app.Fragment;
 
 public class AcasaFragment extends Fragment {
 
+    Button caminAbtn, caminBbtn;
+    RelativeLayout caminArel, caminBrel;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_acasa, container, false) ;
+        View view= inflater.inflate(R.layout.fragment_acasa, container, false);
+
+        caminAbtn = view.findViewById(R.id.buton_A);
+        caminBbtn = view.findViewById(R.id.buton_B);
+        caminArel = view.findViewById(R.id.rel_A);
+        caminBrel = view.findViewById(R.id.rel_B);
+
+        caminArel.setVisibility(View.GONE);
+        caminBrel.setVisibility(View.GONE);
+
+        // Buton Camin A -->  info
+        caminAbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (caminArel.getVisibility() == View.GONE) {
+                    caminArel.setVisibility(View.VISIBLE);
+                }
+                else {
+                    caminArel.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        // Buton Camin B -->  info
+        caminBbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (caminBrel.getVisibility() == View.GONE) {
+                    caminBrel.setVisibility(View.VISIBLE);
+                }
+                else {
+                    caminBrel.setVisibility(View.GONE);
+                }
+            }
+        });
+
+
+
+        return view;
     }
 }
